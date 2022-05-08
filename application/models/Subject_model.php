fc5b7a59b9a94a8fa03a9e82f0240da651323e3b
marzook1999 <77508759+marzook1999@users.noreply.github.com>
2022-05-09 00:01:53 +0530

    Add files via upload

---
+++ application/models/Subject_model.php
@@ -0,0 +1,118 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Subject_model extends MY_Model {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get($id = null) {
+        $this->db->select()->from('subjects');
+        if ($id != null) {
+            $this->db->where('id', $id);
+        } else {
+            $this->db->order_by('id');
+        }
+        $query = $this->db->get();
+        if ($id != null) {
+            return $query->row_array();
+        } else {
+            return $query->result_array();
+        }
+    }
+
+    public function remove($id) {
+		$this->db->trans_start(); # Starting Transaction
+        $this->db->trans_strict(false); # See Note 01. If you wish can remove as well
+        //=======================Code Start===========================
+        $this->db->where('id', $id);
+        $this->db->delete('subjects');
+		$message      = DELETE_RECORD_CONSTANT." On subjects id ".$id;
+        $action       = "Delete";
+        $record_id    = $id;
+        $this->log($message, $record_id, $action);
+		//======================Code End==============================
+        $this->db->trans_complete(); # Completing transaction
+        /*Optional*/
+        if ($this->db->trans_status() === false) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return false;
+        } else {
+        //return $return_value;
+        }
+    }
+
+    public function add($data) {
+		$this->db->trans_start(); # Starting Transaction
+        $this->db->trans_strict(false); # See Note 01. If you wish can remove as well
+        //=======================Code Start===========================
+        if (isset($data['id'])) {
+            $this->db->where('id', $data['id']);
+            $this->db->update('subjects', $data);
+			$message      = UPDATE_RECORD_CONSTANT." On subjects id ".$data['id'];
+			$action       = "Update";
+			$record_id    = $data['id'];
+			$this->log($message, $record_id, $action);
+			//======================Code End==============================
+
+			$this->db->trans_complete(); # Completing transaction
+			/*Optional*/
+
+			if ($this->db->trans_status() === false) {
+				# Something went wrong.
+				$this->db->trans_rollback();
+				return false;
+
+			} else {
+				//return $return_value;
+			}
+        } else {
+            $this->db->insert('subjects', $data);
+            $id = $this->db->insert_id();		
+			$message      = INSERT_RECORD_CONSTANT." On subjects id ".$id;
+			$action       = "Insert";
+			$record_id    = $id;
+			$this->log($message, $record_id, $action);
+			//echo $this->db->last_query();die;
+			//======================Code End==============================
+
+			$this->db->trans_complete(); # Completing transaction
+			/*Optional*/
+
+			if ($this->db->trans_status() === false) {
+				# Something went wrong.
+				$this->db->trans_rollback();
+				return false;
+
+			} else {
+				//return $return_value;
+			}
+			return $id;
+        }
+    }
+
+    function check_data_exists($data) {
+        $this->db->where('name', $data['name']);
+        $query = $this->db->get('subjects');
+        if ($query->num_rows() > 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    function check_code_exists($data) {
+        $this->db->where('code', $data['code']);
+        $query = $this->db->get('subjects');
+        if ($query->num_rows() > 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+}
