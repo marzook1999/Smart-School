643f0ecca9350adbe88880d277b79a4d568df9cd
marzook1999 <77508759+marzook1999@users.noreply.github.com>
2022-05-08 23:32:42 +0530

    Add files via upload

---
+++ application/config/license.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = 'JHK59O79YADC4NUI';
+$config['envato_market_username']      = 'tirah';
+$config['SSLK'] = 'ksokdhiwk72839gs29d3sjd';
